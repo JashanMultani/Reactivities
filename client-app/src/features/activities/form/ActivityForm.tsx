b7436ee0b2c10ae15aadeb683897878940fed9d9
Jashan Multani <jshnmltn8@gmail.com>
2020-08-18 13:33:36 -0400

    End

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,40 +1,49 @@
-import React, { useState, FormEvent, useContext } from 'react';
-import { Segment, Form, Button } from 'semantic-ui-react';
+import React, { useState, FormEvent, useContext, useEffect } from 'react';
+import { Segment, Form, Button, Grid } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 import ActivityStore from '../../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
+import { RouteComponentProps } from 'react-router';
 
-interface IProps {
-  activity: IActivity;
- 
-
+interface DetailParams {
+  id: string;
 }
 
-const ActivityForm: React.FC<IProps> = ({
-  
-  activity: initialFormState,
-  
+const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
+  match,
+  history
 }) => {
   const activityStore = useContext(ActivityStore);
-  const {createActivity, editActivity, submitting, cancelFormOpen} = activityStore;
-  const initializeForm = () => {
-    if (initialFormState) {
-      return initialFormState;
-    } else {
-      return {
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: '',
-        city: '',
-        venue: ''
-      };
-    }
-  };
+  const {
+    createActivity,
+    editActivity,
+    submitting,
+    activity: initialFormState,
+    loadActivity,
+    clearActivity
+  } = activityStore;
 
-  const [activity, setActivity] = useState<IActivity>(initializeForm);
+  const [activity, setActivity] = useState<IActivity>({
+    id: '',
+    title: '',
+    category: '',
+    description: '',
+    date: '',
+    city: '',
+    venue: ''
+  });
+
+  useEffect(() => {
+    if (match.params.id && activity.id.length === 0) {
+      loadActivity(match.params.id).then(
+        () => initialFormState && setActivity(initialFormState)
+      );
+    }
+    return () => {
+      clearActivity()
+    }
+  }, [loadActivity, clearActivity, match.params.id, initialFormState, activity.id.length]);
 
   const handleSubmit = () => {
     if (activity.id.length === 0) {
@@ -42,9 +51,9 @@ const ActivityForm: React.FC<IProps> = ({
         ...activity,
         id: uuid()
       };
-      createActivity(newActivity);
+      createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`))
     } else {
-      editActivity(activity);
+      editActivity(activity).then(() => history.push(`/activities/${activity.id}`));
     }
   };
 
@@ -56,7 +65,9 @@ const ActivityForm: React.FC<IProps> = ({
   };
 
   return (
-    <Segment clearing>
+    <Grid>
+      <Grid.Column width={10}>
+      <Segment clearing>
       <Form onSubmit={handleSubmit}>
         <Form.Input
           onChange={handleInputChange}
@@ -96,16 +107,25 @@ const ActivityForm: React.FC<IProps> = ({
           placeholder='Venue'
           value={activity.venue}
         />
-        <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
         <Button
-          onClick={cancelFormOpen}
+          loading={submitting}
+          floated='right'
+          positive
+          type='submit'
+          content='Submit'
+        />
+        <Button
+          onClick={() => history.push('/activities')}
           floated='right'
           type='button'
           content='Cancel'
         />
       </Form>
     </Segment>
+      </Grid.Column>
+    </Grid>
+    
   );
 };
 
-export default observer (ActivityForm);
+export default observer(ActivityForm);
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './app/layout/styles.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './app/layout/App';
-import 'semantic-ui-css/semantic.min.css'
 import * as serviceWorker from './serviceWorker';
+import ScrollToTop from './app/layout/ScrollToTop';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<BrowserRouter>
+<ScrollToTop>
+<App />
+
+</ScrollToTop>
+
+</BrowserRouter>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
